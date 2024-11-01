894991ad911454e49fdd4875d757f7bd0ac8c2c2
Cameron <camjr246@yahoo.co.uk>
2024-11-01 17:12:27 +0000

    Added example todo item in view

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,3 +7,11 @@ document.getElementById("loadListButton").addEventListener("click", () => {
     document.getElementById("startDiv").setAttribute("hidden", "true");
     document.getElementById("loadListDiv").removeAttribute("hidden");
 });
+
+
+/*
+TODO:
+    - Function to add tasks to view
+    - Function to remove tasks from view
+    - Function to save tasks to database
+*/
