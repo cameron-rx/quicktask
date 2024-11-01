0634266f4de9f48546faefa879f5ff81cad39368
Cameron <camjr246@yahoo.co.uk>
2024-11-01 17:42:35 +0000

    Can add and remove tasks on client side

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,6 +8,40 @@ document.getElementById("loadListButton").addEventListener("click", () => {
     document.getElementById("loadListDiv").removeAttribute("hidden");
 });
 
+document.getElementById("addItemButton").addEventListener("click", addTask);
+
+function addTask() {
+    console.log("Add task");
+    const task = document.getElementById("newItem").value;
+    const newTask = document.createElement("div");
+
+    console.log(task)
+
+    const taskLabel = document.createElement('label');
+    taskLabel.innerHTML = task
+
+    const taskCheckbox = document.createElement('input');
+    taskCheckbox.setAttribute("type", "checkbox");
+
+    const taskRemoveButton = document.createElement('button');
+    taskRemoveButton.innerHTML = "x";
+    taskRemoveButton.addEventListener("click", () =>{
+        removeTask(newTask)
+    });
+
+    newTask.appendChild(taskLabel);
+    newTask.appendChild(taskCheckbox);
+    newTask.appendChild(taskRemoveButton);
+
+    document.getElementById("tasks").appendChild(newTask);
+
+    document.getElementById("newItem").value = "";
+}
+
+function removeTask(parent) {
+    console.log("Remove clicked")
+    parent.remove();
+}
 
 /*
 TODO:
