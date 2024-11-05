9d34dbdfea0a0f8f4822af22be0d867cf73ed596
Cameron <camjr246@yahoo.co.uk>
2024-11-05 14:21:31 +0000

    Added CRUD operations to model, needs testing

--- models/todos.js
+++ models/todos.js
@@ -4,7 +4,8 @@ let Schema = mongoose.Schema;
 
 const todoItemSchema = new Schema({
     name: {type: String, required: true},
-    status: {type: Boolean, required: true}
+    status: {type: Boolean, required: true},
+    completedBy: {type: String, required: false}
 });
 
 const listSchema = new Schema({
@@ -15,4 +16,75 @@ const listSchema = new Schema({
 });
 
 listSchema.set('toObject', {getters: true, virtuals: true});
-module.exports = mongoose.model('todos', listSchema);
+listModel = mongoose.model('todos', listSchema);
+
+
+/* TODO
+- Create list
+- Delete list
+- Update list
+*/ 
+
+exports.create = function(data) {
+    const list = new listModel(
+        {
+            id: data.id,
+            user: data.user,
+            name: data.name,
+            items: data.items
+        }
+    )
+
+    list.save().then(list => {
+        console.log(list)
+        return "List Saved"
+    }).catch(err => {
+        console.log(err)
+        return "Error"
+    })
+}
+
+exports.delete = function(listID) {
+    listModel.deleteOne({id: listID}).then(res => {
+        if (res.n > 0) {
+            console.log("Deleted Document")
+            return "Document Deleted"
+        } else {
+            console.log("Document Not Found")
+            return "Document Not Found"
+        }
+    }).catch(err => {
+        console.log(err)
+        return "Error"
+    })
+}
+
+exports.update = function(listID, newItems) {
+    listModel.updateOne({id: listID}, {items: newItems}).then(res => {
+        if (res.matchedCount > 0) {
+            console.log("Found list to update");
+        } else {
+            console.log("Couldnt find list to update");
+            return "No list found to update"
+        }
+
+        if (res.modifiedCount > 0){
+            console.log("List updated")
+            return "List updated"
+        }
+    }).catch(err => {
+        console.log(err)
+        return err
+    })
+}
+
+exports.get = function(listID) {
+    listModel.findOne({id: listID}).then(list => {
+        console.log("Found List")
+        return list
+    }).catch(err => {
+        console.log(err)
+        return err
+    })
+}
+
